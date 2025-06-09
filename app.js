8d0449fd2ec7bb876a32363d84a1a8c876ad78ed
satyam-0777 <satyamkumarpandey48@gmail.com>
2025-06-09 21:47:49 +0530

    Delete app.js

--- app.js
+++
@@ -1,9 +0,0 @@
-function login() {
-  alert("Login logic placeholder");
-}
-function signup() {
-  alert("Signup logic placeholder");
-}
-function selectSubject(subject) {
-  alert("Selected subject: " + subject);
-}
