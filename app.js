239a0c420cf16d2fb07107895cbbd975b5b7638f
satyam-0777 <satyamkumarpandey48@gmail.com>
2025-06-09 21:31:26 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+function login() {
+  alert("Login logic placeholder");
+}
+function signup() {
+  alert("Signup logic placeholder");
+}
+function selectSubject(subject) {
+  alert("Selected subject: " + subject);
+}
